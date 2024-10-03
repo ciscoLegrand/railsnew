8368350cad143706dbdd017a3221e646ffd6a795
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-10-03 19:08:53 +0200

    🛠️Build pages controller and fix configurations

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,18 +5,23 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/components/*.{erb,haml,html,slim}',
+    './app/components/**/*.{erb,haml,html,slim}',
+    './app/assets/stylesheets/components/**/*.css',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', 'Roboto', ...defaultTheme.fontFamily.sans],
+        mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/container-queries'),
   ]
 }
